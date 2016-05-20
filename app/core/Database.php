c9806e7eb3bfc8d38800fc267e5e120593aed862
Hannes Kindströmmer <me@brolaugh.com>
2016-05-20 14:23:42 +0200

    Added empty two database classes

---
+++ app/core/Database.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hannes.kindstrommer
+ * Date: 2016-05-20
+ * Time: 13:41
+ */
+require_once '../app/core/DatabaseConfig.php';
+
+class Database extends DatabaseConfig
+{
+}
