8ee30ad0767bb9b1442048fceda661482388fa24
Hannes "Brolaugh" Kindströmmer <me@brolaugh.com>
2016-05-28 02:34:43 +0200

    Switched CSS and JS to cookie free webserver

--- app/templates/blog/head.php
+++ app/templates/blog/head.php
@@ -4,7 +4,7 @@
     <meta name="keywords" content="Hello from the others side"/>
     <?= (isset($author->var->name)) ? '<meta name="author" content="' . $author->var->name . '"/>' : false ?>
     <link rel="stylesheet"
-          href="<?= (isset($blog->stylesheet)) ? $blog->stylesheet : "/public/css/material-design.min.css" ?>"
+          href="<?= (isset($blog->stylesheet)) ? $blog->stylesheet : "http://static.brolaugh.com/blog/css/material-design.min.css" ?>"
           charset="utf-8"/>
     <title><?= $blog->title . " - Brolaugh Blog Platform yo" ?></title>
 </head>
