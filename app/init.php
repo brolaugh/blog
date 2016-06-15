7ea82c2aea02493a190fe810a3a664a7bd458380
Hannes "Brolaugh" Kindströmmer <me@brolaugh.com>
2016-06-15 21:04:43 +0200

    Added a modified var_dump function for debugging

--- app/init.php
+++ app/init.php
@@ -4,3 +4,9 @@ require_once 'core/App.php';
 require_once 'core/Controller.php';
 require_once 'core/Database.php';
 require_once '../lib/Parsedown.php';
+
+function var_dumpi($arg){
+    echo '<pre>';
+    var_dump($arg);
+    echo '</pre>';
+}
