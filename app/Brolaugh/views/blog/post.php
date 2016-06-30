d28b86f2119d93592113963c5419d52986a976d0
Hannes "Brolaugh" Kindströmmer <me@brolaugh.com>
2016-06-30 22:03:53 +0200

    Converted /blog/post/* to twig

--- app/Brolaugh/views/blog/post.php
+++ app/Brolaugh/views/blog/post.php
@@ -1,28 +1,21 @@
-<?php
-$blog = $data["blog"];
-$post = $data["post"];
-$author = $data["author"];
-$sideMenuItems = $data['sideMenuItems'];
-$utility = $data['utility'];
-?>
+{% extends 'base.php' %}
+{% block head %}
+  {% include 'blog/head.php'%}
+{% endblock %}
 
-<?php include('../app/Brolaugh/templates/blog/head.php') ?>
-<body>
-<?php include('../app/Brolaugh/templates/blog/header.php') ?>
-<div class="container-fluid">
-    <div class="row">
-        <?php include('../app/Brolaugh/templates/blog/sidemenu.php') ?>
-        <main class="col-md-8">
-            <?php
-            include_once '../app/Brolaugh/templates/blog/post.php';
-            ?>
-        </main>
-        <aside class="col-md-2">
-            Ads
-        </aside>
-    </div>
+{% block body %}
+  {% include 'blog/header.php'%}
+  <div class="container-fluid">
+      <div class="row">
+        {% include 'blog/sidemenu.php'%}
+          <main class="col-md-8">
+            {% include 'blog/post/post.php' %}
+          </main>
+          <aside class="col-md-2">
+              Ads
+          </aside>
+      </div>
 
 
-</div>
-<?php include('../app/Brolaugh/templates/footer.php') ?>
-</body>
+  </div>
+{% endblock %}
