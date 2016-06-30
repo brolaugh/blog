74ed8968962894070a48e25a58a890a8bb6e28a1
Hannes "Brolaugh" Kindströmmer <me@brolaugh.com>
2016-06-30 22:14:06 +0200

    Almost done with /blog/compose

--- app/Brolaugh/views/blog/compose.php
+++ app/Brolaugh/views/blog/compose.php
@@ -1,20 +1,19 @@
-
-<?php include('../app/Brolaugh/templates/blog/head.php') ?>
-<body>
-<?php include('../app/Brolaugh/templates/blog/header.php') ?>
-<div class="container-fluid">
-    <div class="row">
-        <!--Switch out for list of non published and draft posts-->
-        <?php include('../app/Brolaugh/templates/blog/compose/sidemenu.php') ?>
-        <main class="col-md-8">
-            <?php include('../app/Brolaugh/templates/blog/compose.php') ?>
-        </main>
-        <aside class="col-md-2">
-            Ads
-        </aside>
-    </div>
-
-
-</div>
-<?php include('../app/Brolaugh/templates/footer.php') ?>
-</body>
+{% extends 'base.php' %}
+{% block head %}
+  {% include 'blog/head.php'%}
+{% endblock %}
+{% block body %}
+  {% include 'blog/header.php'%}
+  <div class="container-fluid">
+      <div class="row">
+          <!--Switch out for list of non published and draft posts-->
+          {% include 'blog/compose/sidemenu.php'%}
+          <main class="col-md-8">
+            {% include 'blog/compose/form.php' %}
+          </main>
+          <aside class="col-md-2">
+              Ads
+          </aside>
+      </div>
+  </div>
+{% endblock %}
